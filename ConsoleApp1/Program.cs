842a03a759f1144813713cd5ab2f8d7e27e24b13
Anna Eklund <anna.eklund@yahoo.se>
2025-05-06 20:19:32 +0200

    Add variables, input, calculations and blank lines

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,7 +5,24 @@ namespace HelloWorld
   {
     static void Main(string[] args) // this method has to be called Main if it going to be the entrypoint and there is no Top-level statements file
     {
-      Console.WriteLine("Hello, World!");
+      Console.WriteLine("\nHello!\n");
+
+      Console.Write("Please input you name: ");
+      string? userName = Console.ReadLine(); // username can be string or possibly null, Console.ReadLine() returns string generally
+
+      Console.WriteLine("\n\nWelcome" + (string.IsNullOrEmpty(userName) ? "" : " " + userName) + "!\n\n"); //https://stackoverflow.com/questions/26338571/checking-console-readline-null
+      Console.WriteLine("Let figure out you average walking speed!\n");
+
+      Console.Write("Enter a distance in kilometers that you have walked: ");
+      int walkingDistance = Convert.ToInt32(Console.ReadLine()); // https://stackoverflow.com/questions/24443827/reading-an-integer-from-user-input
+      Console.Write("Enter the time in minutes it took to walk that distance: ");
+      int walkingTime = Convert.ToInt32(Console.ReadLine());
+
+      int averageWalkingSpeed = walkingDistance / (walkingTime / 60);
+
+      Console.WriteLine("\n\nYou average walking speed was " + averageWalkingSpeed + "!\n\n"); //https://stackoverflow.com/questions/26338571/checking-console-readline-null
+
+
       Console.WriteLine("Press enter to exit...");
       Console.ReadLine();
     }
