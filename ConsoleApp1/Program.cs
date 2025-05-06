f0c8ab288551ff4e92675138e789d94245a16fa7
Anna Eklund <anna.eklund@yahoo.se>
2025-05-06 22:00:30 +0200

    Revert "Refactor, add private utility function"
    This reverts commit 80445beb3c03a0e742e7fb421c0c96735df887de.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,30 +13,18 @@ namespace HelloWorld
       Console.WriteLine("\n\nWelcome" + (string.IsNullOrEmpty(userName) ? "" : " " + userName) + "!\n\n"); //https://stackoverflow.com/questions/26338571/checking-console-readline-null
       Console.WriteLine("Let figure out you average walking speed!\n");
 
+      double walkingDistance = 0;
 
-
-      double walkingDistance = GetConvertedDoubleInput("Enter a distance in kilometers that you have walked: ");
-      double walkingTime = GetConvertedDoubleInput("Enter the time in minutes it took to walk that distance: ");
-      double averageWalkingSpeed = walkingDistance / (walkingTime / 60);
-
-      Console.WriteLine("\n\nYou average walking speed was " + averageWalkingSpeed + " km/h!\n\n"); //https://stackoverflow.com/questions/26338571/checking-console-readline-null
-
-
-      Console.WriteLine("Press enter to exit...");
-      Console.ReadLine();
-    }
-    private static double GetConvertedDoubleInput(string prompt) // private method for class-scoped clean refactoring, can be changed to public when it is needed outside of this class
-    {
-      double value = 0;
       bool isNotValidInput = true;
       while (isNotValidInput)
       {
-        Console.Write(prompt);
+        Console.Write("Enter a distance in kilometers that you have walked: ");
         string? walkingDistanceInput = Console.ReadLine();
 
 
-        if (double.TryParse(walkingDistanceInput, out value)) // https://stackoverflow.com/questions/14304591/check-if-user-input-is-a-number, out value assigns value with the parsed double if the TryParse was successful
+        if (double.TryParse(walkingDistanceInput, out walkingDistance)) // https://stackoverflow.com/questions/14304591/check-if-user-input-is-a-number
         {
+          walkingDistance = Convert.ToInt32(walkingDistanceInput); // https://stackoverflow.com/questions/24443827/reading-an-integer-from-user-input, double for decimals https://www.w3schools.com/cs/cs_data_types.php
           isNotValidInput = false;
         }
         else
@@ -44,9 +32,35 @@ namespace HelloWorld
           Console.WriteLine("Invalid input. Enter a number or decimal.");
         }
       }
-      return value;
+
+      double walkingTime = 0;
+
+      isNotValidInput = true;
+      while (isNotValidInput)
+      {
+        Console.Write("Enter the time in minutes it took to walk that distance: ");
+        string? walkingTimeInput = Console.ReadLine();
+
+
+        if (double.TryParse(walkingTimeInput, out walkingTime)) // https://stackoverflow.com/questions/14304591/check-if-user-input-is-a-number
+        {
+          walkingTime = Convert.ToInt32(walkingTimeInput); // https://stackoverflow.com/questions/24443827/reading-an-integer-from-user-input, double for decimals https://www.w3schools.com/cs/cs_data_types.php
+          isNotValidInput = false;
+        }
+        else
+        {
+          Console.WriteLine("Invalid input. Enter a number or decimal.");
+        }
+      }
+
+      double averageWalkingSpeed = walkingDistance / (walkingTime / 60);
+
+      Console.WriteLine("\n\nYou average walking speed was " + averageWalkingSpeed + "!\n\n"); //https://stackoverflow.com/questions/26338571/checking-console-readline-null
+
+
+      Console.WriteLine("Press enter to exit...");
+      Console.ReadLine();
     }
   }
-
 
 }
