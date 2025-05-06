1924d43b463916895cac2b73f8152ce5567a71ed
Anna Eklund <anna.eklund@yahoo.se>
2025-05-06 18:41:36 +0200

    Add version with trad boilerplate entrypiont

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace HelloWorld
+{
+  class Program // can technically be called anything but Program is best practice
+  {
+    static void Main(string[] args) // this method has to be called Main if it going to be the entrypoint and there is no Top-level statements file
+    {
+      Console.WriteLine("Hello, World!");
+      Console.WriteLine("Press enter to exit...");
+      Console.ReadLine();
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# dotnet-test-project
+Project in C# for learning and testing.
